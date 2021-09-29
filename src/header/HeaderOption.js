a70bd00da42facc02edf0ccb633d8df8d674ea63
AdegokeJaafar <jaafaradegoke@gmail.com>
2021-09-29 15:47:06 +0100

    Header is done

---
+++ src/header/HeaderOption.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import './HeaderOption.css'
+
+function HeaderOption() {
+    return (
+        <div className="headerOptions">
+            <ul className="headerOption">
+                <li className=" m-3">
+                    Home
+                </li>
+
+                <li className="net m-3">
+                    My Network
+                </li>
+
+                <li className="m-3">
+                    Jobs
+                </li>
+
+                <li className="m-3">
+                    Messaging
+                </li>
+
+                <li className="m-3">
+                    Notifications
+                </li>
+                
+                <li>
+                    <img className="rounded-circle  m-3 img" src="https://image.shutterstock.com/image-photo/surreal-image-african-elephant-wearing-260nw-1365289022.jpg" alt=""  />
+                    
+                </li>
+            </ul>
+            
+        </div>
+    )
+}
+
+export default HeaderOption
