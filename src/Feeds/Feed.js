b7ac5a736de22f7f878ed010150c4f3962f2c180
AdegokeJaafar <jaafaradegoke@gmail.com>
2021-10-07 01:33:33 +0100

    feeds updated

--- src/Feeds/Feed.js
+++ src/Feeds/Feed.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import './Feed.css';
+import InputOption from './InputOption';
 
 function Feed() {
     return (
@@ -12,7 +13,24 @@ function Feed() {
                     <button type="submit">Send</button>
                     </form>
                 </div>
+                <div className="feed-inputOptions">
+                    <img className="photo" src="https://cdn-icons-png.flaticon.com/512/1040/1040241.png" alt=""/>
+                    <InputOption title="Photo" />
+                </div>
+            <div className="feed-inputOptions">
+                    <img className="photo" src="https://cdn-icons-png.flaticon.com/512/1179/1179069.png" alt=""/>
+                    <InputOption title="Video" />
+                </div>
+            <div className="feed-inputOptions">
+                    <img className="photo" src="https://cdn-icons-png.flaticon.com/512/5220/5220135.png" alt=""/>
+                    <InputOption title="Event" />
+                </div>
+            <div className="feed-inputOptions">
+                    <img className="photo" src="https://cdn-icons-png.flaticon.com/512/2680/2680919.png" alt=""/>
+                    <InputOption title="Write article" />
+                </div>
             </div>
+            
         </div>
     )
 }
---
+++ src/Feeds/InputOption.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./InputOption.css";
+
+function InputOption({ title }) {
+    return (
+        <div className="inputOption">
+            <h4>{title}</h4>
+        </div>
+    )
+}
+
+export default InputOption
